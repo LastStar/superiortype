e1356a02e12ef7bf19a5c3cbe168b8956ccfffe9
Josef Pospíšil <josef.pospisil@laststar.eu>
2016-01-20 10:28:01 +0100

    Update to latest and greatest

--- project.clj
+++ project.clj
@@ -1,19 +1,19 @@
 (defproject superiortype "0.3.0-SNAPSHOT"
   :description "Web pages for the Superior Type"
   :url "http://reframed.superiortype.com"
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.145"]
-                 [reagent "0.5.1"]
-                 [re-frame "0.5.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.7.228"]
+                 [reagent "0.6.0-alpha"]
+                 [re-frame "0.7.0-alpha"]
                  [secretary "1.2.3"]
                  [facjure/gardener "0.1.0"]
-                 [garden "1.2.5"]]
+                 [garden "1.3.0"]]
 
   :source-paths ["src/clj"]
 
-  :plugins [[lein-cljsbuild "1.1.0"]
-            [lein-figwheel "0.4.1" :exclusions [cider/cider-nrepl]]
-            [lein-garden "0.2.6"] ]
+  :plugins [[lein-cljsbuild "1.1.2"]
+            [lein-figwheel "0.5.0-3" :exclusions [cider/cider-nrepl]]
+            [lein-garden "0.2.6"]]
 
   :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"
                                     "resources/public/css/compiled/site.css"]
